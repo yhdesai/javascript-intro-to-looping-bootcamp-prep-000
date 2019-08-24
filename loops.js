78e3b48960c15f5b5b929add86972d75fc630272
yash desai <yhdesai99@ymail.com>
2019-08-24 14:04:52 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,13 @@
+function forLoop(array) {
+for (var i = 0; i < 25; i++) {
+  array[i] = "I am ${i} strange loop${i == 0 ? ' ' : 's'}."
+}  
+}
+
+function whileLoop(n) {
+i =10
+  while (i>0) {
+console.log(n);
+    i--
+  }
+}
