529546761d899972546a65e5287942de4f94ac94
yash desai <yhdesai99@ymail.com>
2019-08-24 14:15:05 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,7 +1,6 @@
 function forLoop(array) {
 for (var i = 0; i < 25; i++) {
-  a = i == 0 ? ' ' : 's'
-  array[i] = "I am ${i} strange loop" + a
+  array.push("I am ${i} strange loop${i == 0 ? ' ' : 's'}.")
    }
  return array
 }
